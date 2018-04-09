521bcba4bca5e6fd68d8f08bd4ed0f6c940fe883
ogonzal <oscar.gonzalez@elliemae.com>
2018-04-08 23:16:16 -0700

    everything is ready

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,25 @@
 import React from 'react'
 import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
+import styled from 'styled-components'
+import ProfileImg from '../assets/images/profile-min.jpg'
+
+const SectionHero = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`
 
 class HomePage extends React.Component {
   render() {
   
     return (
       <div>
-        Hhelo there
+        <SectionHero>
+          <img src={ProfileImg} />
+          Hhelo there
+        </SectionHero>
       </div>
     )
   }

470e7cd7bca79284d8c5aa5c99862322de51759c
ogonzal <oscar.gonzalez@elliemae.com>
2018-05-31 19:45:32 -0400

    lowering images sizes

--- .cache/api-runner-ssr.js
+++ .cache/api-runner-ssr.js
@@ -1,5 +1,5 @@
 var plugins = [{
-      plugin: require('/Users/ogonzalez2/Desktop/_DESIGN SYSTEMS/portfolio-2018/ds-portfolio/node_modules/gatsby-plugin-react-helmet/gatsby-ssr.js'),
+      plugin: require('/Users/ogonzalez2/Desktop/_DESIGN-SYSTEMS/portfolio-2018/ds-portfolio/node_modules/gatsby-plugin-react-helmet/gatsby-ssr.js'),
       options: {"plugins":[]},
     }]
 // During bootstrap, we write requires at top of this file which looks like:

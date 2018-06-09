5e2a1806efdcd32b307928858067ff7e26142c6a
ogonzal <oscar.gonzalez@elliemae.com>
2018-06-09 00:07:03 -0700

    ,

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -7,7 +7,6 @@ exports.layouts = {
 }
 
 exports.components = {
-  "component---cache-dev-404-page-js": preferDefault(require("/Users/ogonzalez2/Desktop/_DESIGN-SYSTEMS/portfolio-2018/ds-portfolio/.cache/dev-404-page.js")),
   "component---src-pages-about-js": preferDefault(require("/Users/ogonzalez2/Desktop/_DESIGN-SYSTEMS/portfolio-2018/ds-portfolio/src/pages/about.js")),
   "component---src-pages-index-js": preferDefault(require("/Users/ogonzalez2/Desktop/_DESIGN-SYSTEMS/portfolio-2018/ds-portfolio/src/pages/index.js")),
   "component---src-pages-projects-client-js": preferDefault(require("/Users/ogonzalez2/Desktop/_DESIGN-SYSTEMS/portfolio-2018/ds-portfolio/src/pages/projects/client.js")),
@@ -17,7 +16,6 @@ exports.components = {
 
 exports.json = {
   "layout-index.json": require("/Users/ogonzalez2/Desktop/_DESIGN-SYSTEMS/portfolio-2018/ds-portfolio/.cache/json/layout-index.json"),
-  "dev-404-page.json": require("/Users/ogonzalez2/Desktop/_DESIGN-SYSTEMS/portfolio-2018/ds-portfolio/.cache/json/dev-404-page.json"),
   "about.json": require("/Users/ogonzalez2/Desktop/_DESIGN-SYSTEMS/portfolio-2018/ds-portfolio/.cache/json/about.json"),
   "index.json": require("/Users/ogonzalez2/Desktop/_DESIGN-SYSTEMS/portfolio-2018/ds-portfolio/.cache/json/index.json"),
   "projects-client.json": require("/Users/ogonzalez2/Desktop/_DESIGN-SYSTEMS/portfolio-2018/ds-portfolio/.cache/json/projects-client.json"),

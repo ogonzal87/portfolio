269c0dcdce0596a98fd822eaf791406cef383fb8
ogonzal <oscar.gonzalez@elliemae.com>
2018-07-05 20:02:25 -0300

    about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -17,36 +17,38 @@ const SectionHero = styled.div`
 
 const AboutPage = () => {
   return <div>
-      <TopNav work about resume/>
-      <div className="about-page">
-        <SectionHero>
-          <img src={ProfileImg} alt="profile picture" className="profile-img" />
+    <TopNav work about resume />
+    <div className="about-page">
+      <SectionHero>
 
-          <div className="text-container">
-            <p className=" ">
-              Hi. My name is Oscar and I am addicted to coffee. Although, I blame it on the fact that I was born and raised in Colombia. I am a self-taught designer and developer. I actually went to Business School. Not by choice... 😒 and I hated it. My passion is anything Design and Development related. When I am not creating digital things, I play my uke, ride my bike "La Negra" to get coffee, Latin dance every Wednesday, scuba dive any chance I get and I love nature so much I even trim its bush... Really, I create moss art. Check it out <a href="https://www.instagram.com/musgho/" target="_blank">
-                here
+        <div className="img-container">
+          <h6 className="taking-clients">
+            <i>Currently I am <strong>not</strong> taking on any new projects or clients, but willing to meet for a cup of coffee.</i>
+          </h6>
+          <img src={ProfileImg} alt="profile picture" className="profile-img" />
+        </div>
+
+        <div className="text-container">
+          <p className=" ">
+            Hi. My name is Oscar and I am a Colombian coffee addict (I blame it on my roots). I am also a self-taught designer and developer. I actually went to Business School, not by choice... 😒 and hated it. My passion is anything Design and Development related. When I am not creating digital products, I play my uke, ride my bike "La Negra", Latin dance and scuba dive every chance I get. I love nature so much I even trim its bushes... Literally. Creating moss art is one of my favorite hobbies. Check it out <a href="https://www.instagram.com/musgho/" target="_blank">
+              here
               </a>.
             </p>
-            <br />
-            <br />
-            <br />
-            <p className="special-text-styling">Current Interests</p>
-            <p className="ds-body1-text-style">
-              I am really into decentralized autonomous systems, crypto
-              assets, machine learning and AR.Currently learning while working
-              on a side project coding a DApp, on Ethereum.
+          <br />
+          <br />
+          <br />
+          <p className="special-text-styling">Current Interests</p>
+          <p className="ds-body1-text-style">
+            I am really into decentralized autonomous systems, crypto
+            assets, machine learning and AR.Currently learning while working
+            on a side project coding a DApp using Ethereum.
             </p>
-            <h6 className="taking-clients">
-              <i>
-                Currently <strong>not</strong> taking on any new projects or clients but willing to meet for a cup of coffee.
-              </i>
-            </h6>
-          </div>
-        </SectionHero>
-      </div>
-      <PortfolioFooter />
-    </div>;
+        </div>
+
+      </SectionHero>
+    </div>
+    <PortfolioFooter />
+  </div>;
 }
 
 export default AboutPage
